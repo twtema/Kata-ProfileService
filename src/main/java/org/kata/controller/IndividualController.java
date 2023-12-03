b600d4dbd30084070245f6d3db8b250bec499e8d
Vadim <vzilyas89@gmail.com>
2023-12-03 12:44:27 +0500

    [Task-38]-разрешение конфликта 1

--- src/main/java/org/kata/controller/IndividualController.java
+++ src/main/java/org/kata/controller/IndividualController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jdk.jfr.EventType;
 import lombok.RequiredArgsConstructor;
 import org.kata.dto.IndividualDto;
 import org.kata.exception.IndividualNotFoundException;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 
+@Tag(name = "Individual", description = "The individual API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("v1/individual")
@@ -24,11 +26,49 @@ public class IndividualController {
 
     private final IndividualService individualService;
 
+    @Operation(summary = "Get the Individual")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "The Individual is found",
+                    content = @Content(
+                            mediaType = "Application/JSON",
+                            schema = @Schema(implementation = IndividualDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "The Individual is NOT found",
+                    content = @Content(
+                            mediaType = "Application/JSON",
+                            schema = @Schema(implementation = ErrorMessage.class)
+                    )
+            )
+    })
     @GetMapping
     public ResponseEntity<IndividualDto> getIndividual(@RequestParam String icp) {
         return new ResponseEntity<>(individualService.getIndividual(icp), HttpStatus.OK);
     }
 
+    @Operation(summary = "Create random Individuals by n (count)")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successful Individuals creation",
+                    content = @Content(
+                            mediaType = "Application/JSON",
+                            schema = @Schema(implementation = String.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad Request",
+                    content = @Content(
+                            mediaType = "Application/JSON",
+                            schema = @Schema(implementation = ErrorMessage.class)
+                    )
+            )
+    })
     @PostMapping
     public String createTestIndividual(@RequestParam int n) {
         individualService.createTestIndividual(n);
@@ -41,4 +81,18 @@ public class IndividualController {
         return new ErrorMessage(e.getMessage());
     }
 
+
+    @Operation(summary = "Dedublication, merge Individual")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful deduplication"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    @PostMapping("/deduplication")
+    public ResponseEntity<IndividualDto> dedublication(@RequestBody String icporigin,
+                                                       @RequestBody String icpdedublication,
+                                                       @RequestBody EventType eventType) {
+        return new ResponseEntity<>(individualService.dedublication
+                (icporigin, icpdedublication, eventType), HttpStatus.OK);
+    }
 }
