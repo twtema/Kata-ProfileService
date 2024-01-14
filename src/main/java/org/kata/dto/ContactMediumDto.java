409a15e78c4342f85412970c0afed52e1d08c3de
masamune <emopanda1337@gmail.com>
2024-01-15 01:06:12 +0700

    new field usage

--- src/main/java/org/kata/dto/ContactMediumDto.java
+++ src/main/java/org/kata/dto/ContactMediumDto.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.extern.jackson.Jacksonized;
 import org.kata.dto.enums.ContactMediumType;
+import org.kata.dto.enums.ContactMediumUsage;
 
 
 @Data
@@ -17,8 +18,11 @@ public class ContactMediumDto {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String icp;
 
-    @Schema(description = "Контакты", implementation = ContactMediumType.class, example = "EMAIL")
+    @Schema(description = "Тип контакта", implementation = ContactMediumType.class, example = "EMAIL")
     private ContactMediumType type;
+
+    @Schema(description = "Использование контакта", example = "BUSINESS")
+    private ContactMediumUsage usage;
 
     @Schema(description = "Значение", example = "contact@gmail.com")
     private String value;
