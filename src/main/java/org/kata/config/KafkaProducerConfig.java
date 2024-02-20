d2908728bf5139973ea0fde9e46b7d660d0b35a3
Platon Listkov <platon.listkov@gmail.com>
2024-02-21 00:31:43 +0300

    Task-56 Commit 006 Unused imports have been removed from KafkaProducerConfig.

--- src/main/java/org/kata/config/KafkaProducerConfig.java
+++ src/main/java/org/kata/config/KafkaProducerConfig.java
@@ -2,18 +2,13 @@ package org.kata.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.kata.servlet.LoggableDispatcherServlet;
-import org.kata.servlet.filter.LoggingFilterChain;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
-import org.springframework.web.servlet.DispatcherServlet;
 
 import java.util.HashMap;
 import java.util.Map;
